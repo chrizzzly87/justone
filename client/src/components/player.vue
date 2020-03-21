53978250d98784f4028b24d8ac101dfbddef2a04
Christian.Wisniewski <christian.wisniewski@statista.com>
2020-03-21 10:30:41 +0100

    Added basic lobby functionality

--- client/src/components/player.vue
+++ client/src/components/player.vue
@@ -1,7 +1,21 @@
 <template>
     <div>
-        {{ totalPlayers }}
-        <button @click="addPlayer">Add</button> 
+        <h1>Just One Lobby</h1>
+        <p>
+            ({{ totalPlayers }} players online)
+            <template v-if="allPlayers.length">
+                <ul>
+                    <li v-for="name in allPlayers" :key="name">
+                        {{ name }}
+                    </li>
+                </ul>
+            </template>
+        </p>
+
+        <div>
+            <input type="text" v-model="playerName" placeholder="Playername"/>
+            <button @click="joinGame">Join lobby</button>
+        </div> 
     </div>
 </template>
 
@@ -16,20 +30,30 @@
             return {
                 socket: {},
                 data: {},
+                playerName: '',
                 totalPlayers: 0,
+                allPlayers: [],
             }
         },
         methods: {
-            addPlayer() {
-              this.socket.emit('addPlayer', 1);
+            joinGame() {
+                console.log('emitting join game with: ' + this.playerName);
+                this.socket.emit('joinGame', this.playerName);
             }
         },
         created() {
             this.socket = io('http://localhost:8000');
         },
         mounted() {
-            this.socket.on('players', total => {
-                this.totalPlayers = total;
+            this.socket.on('currentlyOnline', res => {
+                console.log('=> callback for currentlyOnline');
+                console.log(res);
+                this.totalPlayers = res.online;
+            });
+            this.socket.on('allPlayers', res => {
+                console.log('=> callback for allPlayers');
+                console.log(res);
+                this.allPlayers = res.players;
             });
         }
     }
--- index.js
+++ index.js
@@ -14,18 +14,20 @@ app.use(function(req, res, next) {
 
 app.get('/', function (req, res) {
     res.status(200).send('Running JustOne Server');
-
 });
 
-let totalPlayer = 1;
+let players = [];
+let totalPlayer = players.length;
 
 io.on('connection', function (socket) {
     console.log('socket io connection started...');
-    socket.emit('news', { hello: 'world' });
-    console.log('emitting hello world...');
-    socket.on('addPlayer', player => {
-        console.log('getting new player info', addPlayer);
-        totalPlayer += player;
+    socket.emit('currentlyOnline', { online: totalPlayer });
+    socket.on('joinGame', player => {
+        console.log('Player joining the lobby', player);
+        players.push(player);
+        totalPlayer = players.length;
+
+        socket.emit('currentlyOnline', { online: totalPlayer });
+        socket.emit('allPlayers', { players: players });
     });
 });
-
